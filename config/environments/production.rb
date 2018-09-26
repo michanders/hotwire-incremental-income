8bae63fb41525c29e8a481b1ffd709bc6ad24276
michanders <michanders@msn.com>
2018-09-26 15:16:17 -0700

    heroku JavaScript fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,8 +23,16 @@ Rails.application.configure do
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
-  # config.assets.css_compressor = :sass
+  class NoCompression
+       def compress(string)
+           # do nothing
+           string
+       end
+   end
+
+   config.assets.compress = true
+   config.assets.js_compressor = NoCompression.new
+   config.assets.css_compressor = NoCompression.new
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
